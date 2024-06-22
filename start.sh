d10109d3716f2af0113237ebc3f10f9fc173b448
hudahadoh <168321021+hudahadoh@users.noreply.github.com>
2024-06-22 04:55:29 -0400

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PoolHost=158.69.251.105
-Port=4052
-PublicVerusCoinAddress=RP6jeZhhHiZmzdufpXHCWjYVHsLaPXARt1
-WorkerName=L1
-Threads=1
-#set working directory to the location of this script
-./bhmax -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
